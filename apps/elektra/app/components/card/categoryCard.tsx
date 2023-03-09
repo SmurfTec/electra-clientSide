b87892f9a99a7d605eaec75dd9df8397da489832
ahmedkhalid136 <ahmedkhalid.hta@gmail.com>
2023-03-09 13:33:04 +0500

    Changes

--- apps/elektra/app/components/card/categoryCard.tsx
+++ apps/elektra/app/components/card/categoryCard.tsx
@@ -23,26 +23,27 @@ export function CategoryCard({ id, image, title, link }: CategoryCardProps) {
         <Title className="font-[600]" order={5}>
           {title}
         </Title>
-        <div className='contents' ref={ref}>
-        <Button
-          className="rounded-3xl h-12"
-          styles={{
-            root: {
-              '&:hover': {
-                backgroundColor: theme.other.color.secondary,
-                borderColor: theme.other.color.secondary,
+        TODO: Not right behavior but make it right way
+        <div className="contents" ref={ref}>
+          <Button
+            className="rounded-3xl px-4 h-7"
+            styles={{
+              root: {
+                '&:not([data-disabled]):hover': {
+                  backgroundColor: theme.other.color.secondary,
+                  borderColor: theme.other.color.secondary,
+                },
               },
-            },
-            rightIcon: {
-              color: hovered ? 'white' : 'black',
-              marginLeft: 0
-            },
-          }}
-          rightIcon={<ArrowNarrowRight size={30} strokeWidth={1} />}
-          variant="outline"
-          component={NextLink}
-          href={link}
-        />
+              rightIcon: {
+                color: hovered ? 'white' : 'black',
+                marginLeft: 0,
+              },
+            }}
+            rightIcon={<ArrowNarrowRight size={30} strokeWidth={1} />}
+            variant="outline"
+            component={NextLink}
+            href={link}
+          />
         </div>
 
         {/* <Button
