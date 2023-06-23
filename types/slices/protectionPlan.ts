89588729246b90038b50c4d9b4afbdaf943dc3fd
ahmedkhalid136 <ahmedkhalid.hta@gmail.com>
2023-06-23 21:42:44 +0500

    protection plan data added

--- types/slices/protectionPlan.ts
+++ types/slices/protectionPlan.ts
@@ -1,9 +1,9 @@
 export type ProtectionPlan = {
-    id: number;
-    name: string;
-    amount: number;
-    description: string;
-    created_on: string;
-    updated_on: string;
-    month: number;
-}
+  id: number;
+  name: string;
+  amount: number;
+  description: string[];
+  created_on: string;
+  updated_on: string;
+  month: number;
+};
