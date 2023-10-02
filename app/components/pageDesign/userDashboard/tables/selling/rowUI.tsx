cb595c359e0c4e5dc7f3add781a2e62052d2518e
Shehryar_Kiyani <shehryar@xenex-media.com.au>
2023-10-02 17:38:51 +0500

    me api in profile

--- app/components/pageDesign/userDashboard/tables/selling/rowUI.tsx
+++ app/components/pageDesign/userDashboard/tables/selling/rowUI.tsx
@@ -24,7 +24,7 @@ export function ActiveSimpleRow<T extends { id: string | number }>(props: CellCo
               component={NextLink}
               href={`/product-listing/${row.original.id}`}
             >
-              Sell Now
+              Sell Now1
             </Button>
           </Grid.Col>
           <Grid.Col span={2}>
