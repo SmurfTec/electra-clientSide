f7131e0cf6eed63d7b49ffd20057985a3a99be64
Majid Sheikh <majidsheikh0096@gmail.com>
2023-12-30 13:25:34 +0500

    base url updated

--- app/customComponents/utils/http.ts
+++ app/customComponents/utils/http.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse, HttpStatusCode, Method } from 'axios';
 import { getCookie } from 'cookies-next';
 
-export const baseURL = 'http://ec2-3-137-170-245.us-east-2.compute.amazonaws.com:3010';
+export const baseURL = 'http://ec2-3-138-178-31.us-east-2.compute.amazonaws.com:3010';
+
 const httpRequest = axios.create({
   withCredentials: true,
   baseURL,
